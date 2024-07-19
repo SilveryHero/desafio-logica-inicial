48dca0347f60705d0f4c6eea8926496c22900ff7
Silveryhero <silveryhero@gmail.com>
2024-07-19 00:40:00 -0300

    desafio final com prompt

---
+++ desafio-felipao2.js
@@ -0,0 +1,56 @@
+
+let nivel = "nenhum"
+let saldoVitorias = 0
+let vitorias = 69
+let derrotas = 3
+
+function calcularSaldo (vitorias, derrotas) {
+    saldoVitorias = vitorias - derrotas;
+}
+
+function calcularNivel (saldoVitorias) {
+
+    if (saldoVitorias < 10 ) nivel = "Ferro"
+    if (11 < saldoVitorias < 20) nivel = "Bronze"
+    if (21 < saldoVitorias < 50) nivel = "Prata"
+    if (51 < saldoVitorias < 80) nivel = "Ouro"
+    if (81 < saldoVitorias < 90) nivel = "Diamante"
+    if (91 < saldoVitorias < 100) nivel = "Lendário"
+    if (saldoVitorias >= 101)  nivel ="Imortal"
+    
+}
+function printarResultado(){
+    calcularSaldo(vitorias, derrotas);
+    calcularNivel(saldoVitorias);
+
+    switch(nivel){
+        case "Ferro":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Ferro")
+        break;
+
+        case "Bronze":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Bronze")
+        break;
+
+        case "Prata":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Prata")
+        break;
+
+        case "Ouro":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Ouro")
+        break;
+
+        case "Diamante":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Diamante")
+        break;
+
+        case "Lendário":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Lendário")
+        break;
+
+        case "Imortal":
+        console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de Imortal")
+        break;
+    }
+}
+printarResultado();
